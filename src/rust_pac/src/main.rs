b1eaf23b8cb6c786441fd9f9ca25100d59e7085d
thelonesomeprogrammer <m@thelonesomeprogrammer.com>
2025-02-14 13:53:41 +0100

    rust setup and eksample

---
+++ src/rust_pac/src/main.rs
@@ -0,0 +1,50 @@
+use std::sync::{Arc, Mutex};
+use std_msgs::msg::String as StringMsg;
+
+struct RepublisherNode {
+    node: Arc<rclrs::Node>,
+    _subscription: Arc<rclrs::Subscription<StringMsg>>,
+    publisher: Arc<rclrs::Publisher<StringMsg>>,
+    data: Arc<Mutex<Option<StringMsg>>>,
+}
+
+impl RepublisherNode {
+    fn new(context: &rclrs::Context) -> Result<Self, rclrs::RclrsError> {
+        let node = rclrs::Node::new(context, "republisher")?;
+        let data = Arc::new(Mutex::new(None));
+        let data_sub = Arc::clone(&data); 
+        let _subscription = node.create_subscription(
+            "in_topic",
+            rclrs::QOS_PROFILE_DEFAULT,
+            move |msg: StringMsg| {  *data_sub.lock().unwrap() = Some(msg)  },
+        )?;
+        let publisher = node.create_publisher("out_topic", rclrs::QOS_PROFILE_DEFAULT)?;
+        Ok(Self {
+            node,
+            _subscription,
+            publisher,
+            data,
+        })
+    }
+    fn republish(&self) -> Result<(), rclrs::RclrsError> {
+        if let Some(s) = &*self.data.lock().unwrap() {
+            self.publisher.publish(s)?;
+        }
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), rclrs::RclrsError> {
+    let context = rclrs::Context::new(std::env::args())?;
+    let republisher = Arc::new(RepublisherNode::new(&context)?);
+    let republisher_other_thread = Arc::clone(&republisher);
+    std::thread::spawn(move || -> Result<(), rclrs::RclrsError> {
+        loop {
+            use std::time::Duration;
+            std::thread::sleep(Duration::from_millis(1000));
+            republisher_other_thread.republish()?;
+        }
+    });
+    rclrs::spin(Arc::clone(&republisher.node))
+}
+
