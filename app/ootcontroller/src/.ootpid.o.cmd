ecd7bf2c690722f80c6f9879638c0a0d03991188
thelonesomeprogrammer <m@thelonesomeprogrammer.com>
2025-04-21 11:38:10 +0200

    submoduels

---
+++ app/ootcontroller/src/.ootpid.o.cmd
@@ -0,0 +1,67 @@
+cmd_/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.o := arm-none-eabi-gcc -Wp,-MD,/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/.ootpid.o.d     -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src -D__firmware__ -fno-exceptions -Wall -Wmissing-braces -fno-strict-aliasing -ffunction-sections -fdata-sections -Wdouble-promotion -std=gnu11 -DCRAZYFLIE_FW   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/CMSIS/CMSIS/Core/Include   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/CMSIS/CMSIS/DSP/Include   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/libdw1000/inc   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/include   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/portable/GCC/ARM_CM4F   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/config   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/platform/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/deck/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/deck/drivers/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/drivers/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/drivers/bosch/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/drivers/esp32/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/hal/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/kalman_core   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/lighthouse   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/outlierfilter   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/cpx   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/p2pDTR   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/controller   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/estimator   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/utils/interface   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/utils/interface/kve   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/utils/interface/lighthouse   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/utils/interface/tdoa   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/lib/FatFS   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/lib/CMSIS/STM32F4xx/Include   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/lib/STM32_USB_Device_Library/Core/inc   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/lib/STM32_USB_OTG_Driver/inc   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/lib/STM32F4xx_StdPeriph_Driver/inc   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/lib/vl53l1   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/lib/vl53l1/core/inc   -I/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/build/include/generated -fno-delete-null-pointer-checks -Wno-unused-but-set-variable -Wno-unused-const-variable -fomit-frame-pointer -fno-var-tracking-assignments -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -Werror=implicit-int -Werror=date-time -DCC_HAVE_ASM_GOTO -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -g3 -fno-math-errno -DARM_MATH_CM4 -D__FPU_PRESENT=1 -mfp16-format=ieee -Wno-array-bounds -Wno-stringop-overread -Wno-stringop-overflow -DSTM32F4XX -DSTM32F40_41xxx -DHSE_VALUE=8000000 -DUSE_STDPERIPH_DRIVER -Os -Werror   -c -o /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.o /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.c
+
+source_/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.o := /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.c
+
+deps_/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.o := \
+  /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdbool.h \
+  /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdint.h \
+  /usr/arm-none-eabi/include/stdint.h \
+  /usr/arm-none-eabi/include/machine/_default_types.h \
+  /usr/arm-none-eabi/include/sys/features.h \
+  /usr/arm-none-eabi/include/_newlib_version.h \
+  /usr/arm-none-eabi/include/sys/_intsup.h \
+  /usr/arm-none-eabi/include/sys/_stdint.h \
+  /usr/arm-none-eabi/include/string.h \
+  /usr/arm-none-eabi/include/_ansi.h \
+  /usr/arm-none-eabi/include/newlib.h \
+  /usr/arm-none-eabi/include/sys/config.h \
+    $(wildcard include/config/h//.h) \
+  /usr/arm-none-eabi/include/machine/ieeefp.h \
+  /usr/arm-none-eabi/include/sys/reent.h \
+  /usr/arm-none-eabi/include/_ansi.h \
+  /usr/lib/gcc/arm-none-eabi/14.2.0/include/stddef.h \
+  /usr/arm-none-eabi/include/sys/cdefs.h \
+  /usr/arm-none-eabi/include/sys/_types.h \
+  /usr/arm-none-eabi/include/machine/_types.h \
+  /usr/arm-none-eabi/include/sys/lock.h \
+  /usr/arm-none-eabi/include/sys/_locale.h \
+  /usr/arm-none-eabi/include/strings.h \
+  /usr/arm-none-eabi/include/sys/string.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/app.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/include/FreeRTOS.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/config/FreeRTOSConfig.h \
+    $(wildcard include/config/h.h) \
+    $(wildcard include/config/debug/queue/monitor.h) \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/config/config.h \
+    $(wildcard include/config/h/.h) \
+    $(wildcard include/config/block/address.h) \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/drivers/interface/nrf24l01.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/drivers/interface/nRF24L01reg.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/config/trace.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/hal/interface/usec_time.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/utils/interface/cfassert.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/include/projdefs.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/include/portable.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/include/deprecated_definitions.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/include/mpu_wrappers.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/include/task.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/vendor/FreeRTOS/include/list.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/param.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/param_logic.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/crtp.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/utils/interface/debug.h \
+    $(wildcard include/config/debug/print/on/uart1.h) \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/config/config.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/console.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/utils/interface/eprintf.h \
+  /usr/lib/gcc/arm-none-eabi/14.2.0/include/stdarg.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/controller/controller.h \
+    $(wildcard include/config/controller/oot.h) \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/modules/interface/stabilizer_types.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/hal/interface/imu_types.h \
+  /home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/src/utils/interface/lighthouse/lighthouse_types.h \
+
+/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.o: $(deps_/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.o)
+
+$(deps_/home/marrinus/unistuff/4semester/p4/repo/crazyflie-firmware/app/ootcontroller/src/ootpid.o):
